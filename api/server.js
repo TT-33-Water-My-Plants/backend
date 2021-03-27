bf79787abf72412ec72c8741d09b21e45b3594a5
paulstgermain <paulg.stgermain@gmail.com>
2021-03-27 19:09:56 -0400

    completes basic setup

--- api/server.js
+++ api/server.js
@@ -7,4 +7,8 @@ server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
+server.use('/', (req, res, next) => { //eslint-disable-line
+    res.json({ message: 'It\'s working!' });
+})
+
 module.exports = server
