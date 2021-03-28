1954c95f1d95b156c75466d19e17eb30bcdd1795
paulstgermain <paulg.stgermain@gmail.com>
2021-03-27 23:44:14 -0400

    scaffolds user endpoints / db functions

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,14 +1,32 @@
 const db = require('../../data/db-config');
 
 const find = () => {
-    return db('users').select('username')
-}
+    return db('users').select('username', 'phoneNumber');
+};
+
+const findById = (id) => {
+    return db('users')
+        .where('user_id', id)
+        .select('username', 'phoneNumber')
+        .first();
+};
 
 const add = async (user) => {
-    return await db('users').insert(user)
+    return await db('users').insert(user, ['username', 'phoneNumber']);
+};
+
+const update = async (id, changes) => {
+    return await db('users').where('user_id', id).update(changes, ['username', 'phoneNumber']);
+}
+
+const remove = async (id) => {
+    return await db('users').where('user_id', id).del();
 }
 
 module.exports = {
     find,
-    add
+    findById,
+    add,
+    update,
+    remove
 }
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,6 +3,7 @@ const db = require('../../data/db-config');
 const User = require('./users-model');
 
 const express = require('express');
+const { json } = require('express');
 
 const router = express.Router();
 
@@ -14,11 +15,31 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            res.json(user);
+        })
+})
 
 router.post('/', (req, res) => {
     User.add(req.body)
         .then(user => {
-            res.json(user);
+            res.status(201).json(user);
+        })
+})
+
+router.put('/:id', (req, res) => {
+    User.update(req.params.id, req.body)
+        .then(updated => {
+            res.status(200).json(updated);
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    User.remove(req.params.id)
+        .then(deleted => {
+            res.json(deleted);
         })
 })
 
